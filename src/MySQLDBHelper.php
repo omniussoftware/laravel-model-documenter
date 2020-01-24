3df7265afe330318e0c583293e0b4a5d67791325
Lars Bergvall <lars.bergvall@nti.se>
2020-01-24 12:56:39 +0100

    Added backticks around table name. Added support for json columns

--- src/MySQLDBHelper.php
+++ src/MySQLDBHelper.php
@@ -12,7 +12,7 @@ class MySQLDBHelper implements DBHelper {
 	 * { @inheritDoc }
 	 */
 	public function fetchColumnData(string $table): array {
-		return DB::select("DESCRIBE $table");
+		return DB::select("DESCRIBE `$table`");
 	}
 
 	/**
@@ -26,9 +26,10 @@ class MySQLDBHelper implements DBHelper {
 		if (Str::contains($mysqlType, 'int')) {
 			$phpType = 'int';
 		} elseif (Str::contains($mysqlType, 'varchar')
-				|| Str::contains($mysqlType, 'text')
-				|| Str::contains($mysqlType, 'char')
-				|| Str::contains($mysqlType, 'enum')) {
+			|| Str::contains($mysqlType, 'text')
+			|| Str::contains($mysqlType, 'char')
+			|| Str::contains($mysqlType, 'json')
+			|| Str::contains($mysqlType, 'enum')) {
 			$phpType = 'string';
 		} elseif (Str::contains($mysqlType, 'timestamp') || Str::contains($mysqlType, 'datetime')) {
 			$phpType = 'Carbon';
